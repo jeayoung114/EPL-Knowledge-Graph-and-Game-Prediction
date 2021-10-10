1ec3c6cd80d9ea3fde1bd75d696af63b2615b66e
jeayoung114 <jeayoung114@naver.com>
2021-10-09 23:24:07 -0700

    world_football_crawler

---
+++ worldfootball_crawler/worldfootball_crawler/spiders/4_team_info_crawler.py
@@ -0,0 +1,46 @@
+import scrapy
+import logging
+import pandas as pd
+import fileinput
+import json
+import re
+
+class name_crawler(scrapy.Spider):
+    name = "team_info"
+
+    def __init__(self, date='', **kwargs):
+        super().__init__(**kwargs)
+        self.allowed_domain = ['http://www.worldfootball.net']
+
+        df = pd.DataFrame()
+        with fileinput.input(
+                files='/Users/jaeyoungkim/Desktop/usc/DSCI - 558/project/github/Untitled/worldfootball_crawler/player_worldfootball.jl') as file:
+            for line in file:
+                conv = json.loads(line)
+                df = df.append(conv, ignore_index=True)
+
+        self.start_urls = list(df["team_url"].drop_duplicates())
+        # self.start_urls = ["https://www.worldfootball.net/teams/everton-fc/"]
+
+        self.handle_httpstatus_list = [404]
+
+    def parse(self, response):
+        logging.info("response.status:%s" % response.status)
+        res_dict = dict()
+        res = response.css("div[class='sidebar'] table[class='standard_tabelle yellow'] tr")
+        name = response.css("div[class='sidebar'] div[class='box emblemwrapper'] div[class='head'] h2 ::text").getall()[0].strip()
+        res_dict["name"] = name
+
+        for idx, post in enumerate(response.css("div[class='sidebar'] table[class='standard_tabelle yellow'] tr")):
+            # print(post)
+            tmp = post.css("::text").getall()
+            tmp = [re.sub("[\n\t, :]", "", i) for i in tmp]
+            if len(tmp) == 0:
+                pass
+            else:
+                key_val = [i for i in tmp if i != ""]
+                if key_val[1:] != []:
+                    res_dict[key_val[0]] = key_val[1:]
+
+            if idx == len(res)-1:
+                yield res_dict
